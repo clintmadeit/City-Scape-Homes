ab2ad200b10d1912019e5518c075a62e1992f52f
clintmadeit <clintonbrirdley@gmail.com>
2024-03-08 22:38:11 +0300

    Database connection

--- api/index.js
+++ api/index.js
@@ -1,8 +1,20 @@
 import { log } from "console";
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    log("Database is connected successfully!");
+  })
+  .catch((err) => {
+    log("Database connection failed!", err);
+  });
 
 const app = express();
 
 app.listen(3000, () => {
-  log("Server is running on port 3000");
+  log("Server is running on port 3000!");
 });

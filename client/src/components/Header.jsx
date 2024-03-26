420251b2334667e2d45f6907edbe673161c5a143
clintmadeit <clintonbrirdley@gmail.com>
2024-03-26 12:48:18 +0300

    updated header search form functionality

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,23 +1,50 @@
 import { FaSearch } from "react-icons/fa";
 import "../index.css";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { useSelector } from "react-redux";
+import { useEffect, useState } from "react";
 
 export default function Header() {
   const { currentUser } = useSelector((state) => state.user);
+  const [searchTerm, setSearchTerm] = useState("");
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const urlParams = new URLSearchParams(location.search);
+    const searchTermFromUrl = urlParams.get("searchTerm");
+    if (searchTermFromUrl) {
+      setSearchTerm(searchTermFromUrl);
+    }
+  }, [location.search]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const urlParams = new URLSearchParams(window.location.search);
+    urlParams.set("searchTerm", searchTerm);
+    const searchQuery = urlParams.toString();
+    navigate(`/search?${searchQuery}`);
+  };
+
   return (
     <header className="bg-slate-200 shadow-md">
       <div className="flex justify-between items-center max-w-6xl mx-auto">
         <Link to={"/"}>
           <img src="../images/logo.png" alt="Logo" className="logo-img" />
         </Link>
-        <form className="bg-slate-100 p-3 rounded-lg flex items-center">
+        <form
+          onSubmit={handleSubmit}
+          className="bg-slate-100 p-3 rounded-lg flex items-center"
+        >
           <input
             type="text"
             placeholder="Search..."
             className="bg-transparent focus:outline-none w-24 sm:w-48 md:w-96 lg:w-96 xl:w-96"
+            value={searchTerm}
+            onChange={(e) => setSearchTerm(e.target.value)}
           />
-          <FaSearch className="text-neonorange" />
+          <button>
+            <FaSearch className="text-neonorange" />
+          </button>
         </form>
         <ul className="flex gap-4 cursor-pointer">
           <Link to={"/"}>

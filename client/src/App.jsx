7b1b68c2984c71421c710d5455253db8c4589815
clintmadeit <clintonbrirdley@gmail.com>
2024-04-11 19:03:38 +0300

    admin required removed for test

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -33,15 +33,10 @@ export default function App() {
 
         <Route element={<PrivateRoute />}>
           <Route path="/profile" element={<Profile />} />
-          <Route
-            path="/create-listing"
-            element={<CreateListing />}
-            adminRequired
-          />
+          <Route path="/create-listing" element={<CreateListing />} />
           <Route
             path="/update-listing/:listingId"
             element={<UpdateListing />}
-            adminRequired
           />
         </Route>
       </Routes>

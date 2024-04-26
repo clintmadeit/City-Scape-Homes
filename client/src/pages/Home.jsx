8aa3c690c2975964a514ed01e826a6db7a99a564
clintmadeit <clintonbrirdley@gmail.com>
2024-04-26 21:11:37 +0300

    a touch on home page (show more hotels)

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -197,7 +197,7 @@ export default function Home() {
                 className="text-egyptianblue text-sm hover:text-neonorange"
                 to={"/search?type=sale"}
               >
-                Show more hotel listings
+                Show more hotels
               </Link>
             </div>
             <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4">
